fae17477e62e6af9fd623fc87c80abfd06f35b7a
root <root@SOMETHING.localdomain>
2025-08-26 14:48:38 +0300

    keep order

--- ppolynomials.py
+++ ppolynomials.py
@@ -12,13 +12,19 @@ LEN = 3
 
 BASIS_Fpt = [t**i for i in range(p)]
 
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
 def get_random_A_subset(basis, n, len):
-    Bn = {b**e for b, e in itertools.product(basis, range(p**n))}
+    Bn = f7((b**e for b, e in itertools.product(basis, range(p**n))))
     return list(sample(list(Bn),k=len)
 )
 
 def generate_ppolynomial(n, len):
     A = get_random_A_subset(BASIS_Fpt, n, len)
+    print(A)
     return sum([a*symbols(f"x_{i}")**(p**n) for i, a in enumerate(A)])
 
 if __name__ == "__main__":
