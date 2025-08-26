a394c0c7a05c3613da982b37dd32141f47dcbdda
root <root@SOMETHING.localdomain>
2025-08-26 21:37:13 +0300

    .

--- ppolynomials.py
+++ ppolynomials.py
@@ -4,13 +4,14 @@ import itertools
 from random import seed, sample
 from typing import List
 from chatgpt_util import eliminate_single_variable_polys
-seed(1)
+import math
+seed(11)
 t = symbols('t')
 
 p = 3
 Fp = GF(p)
 POWER = 1
-NUM_OF_VAR = 4
+NUM_OF_VAR = 2
 
 BASIS_Fpt = [t**i for i in range(p)]
 
@@ -33,23 +34,26 @@ def generate_ppolynomial(n, len):
 
     return sum([a*symbols(f"x_{i}")**(p**n) for i, a in enumerate(A)])
 
-def generate_iteration(polynomial: Poly):
+def generate_iteration(polynomial):
     """
     Take polynomial and return a system of equations by the algorithm for the Ext group
     """
     polynomial_system = []
     P = Poly(polynomial, *sorted(polynomial.free_symbols, key=lambda s: s.name))
     max_each_index = tuple(map(max, zip(*P.monoms())))[1:]
-    N = max(max_each_index)//p
+    N = int(math.log(int(max(max_each_index)), p))
     for i in range(len(max_each_index)): # number of variables
-        Ni = max_each_index[i]//p
+        Ni = int(math.log(int(max_each_index[i]), p))
         for l in range(p**(N-Ni)):
             polynomial_system.append(create_polynomial(i, N, Ni, l, polynomial))
-    return eliminate_single_variable_polys(polynomial_system)[0]
+    print(f"{polynomial_system=}")
+    eliminated = eliminate_single_variable_polys(polynomial_system)[0]
+    return [x for x in eliminated if x!=0]
 
 def get_c_ijk(polynomial, i, j, k):
     parts = collect(polynomial, symbols(f"x_{i}"), evaluate=False)
     cij = parts.get(symbols(f"x_{i}") ** (p ** k), 0)
+    print(f"{cij=}")
     if collect(cij, t, evaluate=False).get(t ** j, 0) not in [0,1]:
         print("problemo!")
     return collect(cij, t, evaluate=False).get(t ** j, 0) # add root of p^{....}
@@ -62,20 +66,41 @@ def create_polynomial(i, N, Ni, l, old_polynomial):
             if c_ijk != 0:
                 for r in range(p**N):
                     if (r+j+1-(l+1)*p**k) % (p**(k+N-Ni)) == 0:
-                        polynomial += c_ijk*(symbols(f"lam_{r}")**(p**(Ni-k)))*t**((r+j+1-(l+1)*p**k)//(p**(k+N-Ni)))
+                        polynomial += c_ijk*(symbols(f"x_{r}")**(p**(Ni-k)))*t**((r+j+1-(l+1)*p**k)//(p**(k+N-Ni)))
     return polynomial
 
+def dimension_formula(polynomial):
+    P = Poly(polynomial, *sorted(polynomial.free_symbols, key=lambda s: s.name))
+    max_each_index = tuple(map(max, zip(*P.monoms())))[1:]
+    N = int(math.log(int(max(max_each_index)), p))
+    # print(f"N of {polynomial} in {N}")
+    final = p**N
+    for i in range(len(max_each_index)): # number of variables
+        Ni = int(math.log(int(max_each_index[i]), p))
+        final -= p**(N-Ni)
+    return final
+
+
+
 def iterate_twice_check_for_non_stabilizing(P):
+    first_dim = dimension_formula(P)
     first_iter = generate_iteration(P)
+    print(first_iter)
     if len(first_iter) != 1:
         print("problemo!!")
     else:
-        second_iter = generate_iteration(first_iter[0])
-        return second_iter
+        second_iter = generate_iteration(first_iter[0]) # TODO: not always one equation 
+        if len(second_iter) == 1:
+            if first_dim != dimension_formula(second_iter[0]):
+                print("found it!")
+                print(first_dim, dimension_formula(second_iter[0]))
+                print(first_iter)
+                print(second_iter[0])
+                print(generate_iteration(second_iter[0]))
 
 if __name__ == "__main__":
     P = generate_ppolynomial(POWER, NUM_OF_VAR) + symbols("x_0")
     x, y = symbols("x_0 x_1")
     # P = x+ t*x**p + y**p 
     print(f"starting with {P}")
-    print(generate_iteration(P))
+    iterate_twice_check_for_non_stabilizing(P)
